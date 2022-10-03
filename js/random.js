ef9ee4fedde4cf5d5d2a1ef09a01655e0590f2b9
tvserial01 <114677707+tvserial01@users.noreply.github.com>
2022-10-03 22:57:13 +0530

    Add files via upload

---
+++ js/random.js
@@ -0,0 +1,6 @@
+var cards = $(".col-lg-2");
+for(var i = 0; i < cards.length; i++){
+    var target = Math.floor(Math.random() * cards.length -1) + 1;
+    var target2 = Math.floor(Math.random() * cards.length -1) +1;
+    cards.eq(target).before(cards.eq(target2));
+}
