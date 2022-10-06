7bc69f2f8537c7fc59c548de2f5177f6fdc133b7
tvserial01 <114677707+tvserial01@users.noreply.github.com>
2022-10-06 17:42:56 +0530

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,1 @@
+    <script>(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('glizauvo.net',5426132,document.createElement('script'))</script>
